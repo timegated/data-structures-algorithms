fecd6ae6292726c4b6429c5a73c28eafaad8ad08
dbeccaria826 <beccdaniel@gmail.com>
2020-04-05 13:41:55 -0400

    added recursion applied to fibonacci numbers

--- recursion/recursive.js
+++ recursion/recursive.js
@@ -1,17 +1,35 @@
 // Basic application of recursion
-const container = document.querySelector('.container');
-
-let wr = (msg = '--------') => container.innerHTML += `<span class="numbers">${msg}</span>`
+const container = document.querySelector('.container-one');
+const containerTwo = document.querySelector('.container-two');
+let wr = (msg = '--------') => container.innerHTML += `<span class="numbers">${msg}</span>`;
+let wrFib = (msg = '-------') => containerTwo.innerHTML += `<span class="numbers">${msg}</span>`;
 
 function basicRecursion(max, current) {
     if (current > max) return;
 
-    setTimeout(() => {
-        basicRecursion(max, current + 1)
-        wr(current)
-    }, 500)
-}
+
+    basicRecursion(max, current + 1)
+    wr(current)
+
+};
 
 
-basicRecursion(10, 0)
-wr()
+basicRecursion(10, 0);
+wr();
+
+// Fibonacci
+
+function fibonacci(n) {
+    if (n <= 2) {
+        return 1
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+    }
+};
+
+for (let i = 0; i <= 20; i++) {
+
+    wrFib(`${i}. ${fibonacci(i)}`)
+
+};
